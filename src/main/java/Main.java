a4348486bae48a47f564caa7f8066735d32fa7c3
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2019-10-20 18:15:11 +0300

    small fix
    small fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 package main.java;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-       SuperController superController = new SuperController();
-       superController.start();
+//       SuperController superController = new SuperController();
+//       superController.start();
+
+//    byte b1 = 10;
+//    byte b2 = 20;
+//    byte b3 = b1 + b2;
+        int [] myArray = {1,2,3,4};
+        System.out.println(Arrays.toString(myArray));
+
     }
 }
